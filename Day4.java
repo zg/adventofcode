acaf34a1327aa367875c278f6f961ce4fa115ac9
Zack Zatkin-Gold <zzatkin@gmail.com>
2015-12-04 13:34:29 -0500

    move input files, make Day4 code a little bit cleaner

--- Day4.java
+++ Day4.java
@@ -3,30 +3,24 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Day4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         String input = "ckczppom";
         boolean flag = false;
-        try {
             MessageDigest m = MessageDigest.getInstance("MD5");
-            int x = 0;
-            for(; ; x++) {
-                String plaintext = input + Integer.toString(x);
-                m.reset();
-                m.update(plaintext.getBytes());
-                byte[] digest = m.digest();
+            int count = 0;
+            for(; ; count++) {
+                byte[] digest = m.digest((input + Integer.toString(count)).getBytes());
                 BigInteger bigInt = new BigInteger(1, digest);
                 String hashText = bigInt.toString(16);
                 while(hashText.length() < 32)
                     hashText = "0" + hashText;
                 if(hashText.substring(0,5).equals("00000") && !flag) {
                     flag = true;
-                    System.out.println(x);
+                    System.out.println(count);
                 }
                 if(hashText.substring(0,6).equals("000000"))
                     break;
             }
-            System.out.println(x);
-        } catch(NoSuchAlgorithmException nae) {
-        }
+            System.out.println(count);
     }
 }
